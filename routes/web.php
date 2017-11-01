b124df075e0866a43b9c2ff1de045328d1322c26
Luis Enrique Condori Abrigo <luisabrigo@hotmail.com>
2017-10-31 20:44:27 -0500

    definiendo rutas y controlador para el listado, creado y guardado de productos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//para ver la lista se hara peticion get
+Route::get('/admin/products', 'ProductController@index'); //esta ruta mostrara el listado de productos. El controlador que nos permitira gestionar la informacion de los productos sera ProductController, el metodo index sera el encargado de devolver el listado de productos 
+//para crear un producto haremos la peticiuon get para que nos traiga el formulario para agregar el producto
+Route::get('/admin/products/create', 'ProductController@create'); //esta ruta permitira registrar nuevos productos' El controlador usar el metodo create
+//para guardar (registrar) haremos una peticion post a la ruta admi/products
+Route::post('/admin/products', 'ProductController@store'); //para guardar los datos usaremos el metodo sotr en el ProductController
+
+/* terminando todo esto crearemos el controlador ProductoController
+con el comando: php artisan make:controller ProductController
