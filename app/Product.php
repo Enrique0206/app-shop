8bd024bd06cef2984a01ca073edce7ef0dad0b7b
Luis Enrique Condori Abrigo <luisabrigo@hotmail.com>
2017-11-22 15:43:38 -0500

    Accesor para destacar la imagen

--- app/Product.php
+++ app/Product.php
@@ -16,6 +16,20 @@ class Product extends Model
 	{
 		return $this->hasMany(ProductImage::class); //esto tiene muchas imagenes
 	}
+	
+	public function getFeaturedImageUrlAttribute() //se escribe igual que el nombmbre en la line 77de welcome.blade.php (featured_image_url)
+	{
+		$featuredImage = $this->images()->where('featured', true)->first();
+		if($featuredImage)
+			$featuredImage = $this->images()->first();
+		
+		
+		if($featuredImage){
+			return $featuredImage->url;
+		}
+		
+		return '/images/products/default.png'; //retorna la imagen default.jpg  (cuando no haya imagenes que mostrar)
+	}
 }
 
 
